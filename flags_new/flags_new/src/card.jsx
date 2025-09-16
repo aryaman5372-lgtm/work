1fb4971f6086b73b67d06d36f056ff7a185bdb9c
Hunterwork <aryaman5372@gmail.com>
2025-09-16 16:42:36 +0530

    update to make new branch

--- flags_new/flags_new/src/card.jsx
+++ flags_new/flags_new/src/card.jsx
@@ -1,7 +1,6 @@
 
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import './card.css'
 
 const Card  = () => {
     const [flag, setFlag] = useState(null)
---
+++ flags_new/flags_new/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Card from './card'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <h2>fsf</h2>
+<Card/>
+
+
+  </StrictMode>,
+)
